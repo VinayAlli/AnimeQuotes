42294e0d0194cfb88f1a546cda29fa3fa4254d05
VinayAlli <allivinay2001@gmail.com>
2023-06-27 02:43:22 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState,useEffect,React } from "react";
+import axios from 'axios';
+import AOS from 'aos';
 
+AOS.init();
 function App() {
+  const [name,setName]=useState('');
+  const [character,setCharacter]=useState('')
+  const [quote,setQuote]=useState('')
+  const link="https://animechan.xyz/api/random";
+  const Submit=()=>{
+    axios.get(link).then((res)=>{
+      setName(res.data.anime);
+      setCharacter(res.data.character)
+      setQuote(res.data.quote)
+    });
+  }
+  useEffect(()=>{
+    Submit();
+  },[])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='quote-container'>
+    
+    <button className='button' onClick={Submit}>Random Anime Quote</button>
+    <div className='anime-name'>{name}</div>
+    <div className='character'>{character}</div>
+    <div className='quote'>{quote}</div>
     </div>
   );
 }
